a74e636a6e73e959fefb41789ae71e305a5e4a4a
Aneta Pospiech <aneta.pospiech.ai@gmail.com>
2022-12-01 14:50:58 +0100

    open webDriver and pages

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,16 +1,16 @@
 import com.google.common.annotations.VisibleForTesting;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
 public class SeleniumTest {
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
-    }
 
  @Test
-    public void sampleTest() {
-        System.out.println("Hello!");
+    public void openGooglePage() {
+     System.setProperty("webdriver.chrome.driver","/Users/anetapospiech/Downloads/chromedriver");
+     WebDriver driver = new ChromeDriver();
+driver.get("https://www.google.com");
     }
 }
